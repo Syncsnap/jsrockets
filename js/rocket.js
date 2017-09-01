d6204c036c882b7de6d572d49842d4e7258aa96d
Jesper Kollaard <jpuntkollaard@gmail.com>
2017-09-01 20:20:46 +0200

    fixed persistent text

--- js/rocket.js
+++ js/rocket.js
@@ -1,22 +1,36 @@
+
+
+
+
+//set variables 
 var timer = null;
 var countdownNumber = 10;
+
+//functie aanmaken 
 var changeState = function(state) {
 	document.body.className = 'body-state' + state ;
 	clearInterval(timer);
 	countdownNumber = 10;
 	document.getElementById('countdown').innerHTML = countdownNumber;
+	//Vraag iedere ronde de classname op.
+	document.getElementById('nervous').className = 'nervous';
+	document.getElementById('cant-wait').className = 'cant-wait';
 	//countdown
+	
 	if (state == 2) {
 		timer = setInterval(function(){
 			countdownNumber = countdownNumber - 1;
 			document.getElementById('countdown').innerHTML = countdownNumber;
 			
+			// functie voor het flashen van een beeld op het scherm.
 			if (countdownNumber > 4 && countdownNumber <=7) {
 				//be nervous
 				document.getElementById('nervous').className = 'nervous show';
-			} else {
+			} 
+
+			else {
 				document.getElementById('nervous').className = 'nervous';
-			}
+			} 
 
 			if (countdownNumber > 1 && countdownNumber <=4) {
 				//can't wait
@@ -24,7 +38,6 @@ var changeState = function(state) {
 			} else {
 				document.getElementById('cant-wait').className = 'cant-wait';
 			}
-
 			if (countdownNumber <= 0) {
 				changeState(3);
 			};
